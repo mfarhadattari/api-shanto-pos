560261191ce51727d8ed63ec298f1aa5a882bf0c
Mohammad Farhad <mfarhad.attari@gmail.com>
2024-01-05 22:40:48 +0600

    handling unhandled and uncaught error  implemented

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,13 +1,14 @@
 /* eslint-disable no-console */
+import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 import { config } from './config';
-
+let server: Server;
 // -------->> bootstrap server function <<----------------
 const bootstrap = async () => {
   try {
     // listing app
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`[Server] Server listening 🤖`);
     });
 
@@ -21,3 +22,18 @@ const bootstrap = async () => {
 };
 
 bootstrap();
+
+// -------->> handling unhandled and uncaught error <<----------------
+process.on('unhandledRejection', () => {
+  console.log(`[Server] Unhandled Rejection is detected,  shutting down...⚡`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+});
+
+process.on('uncaughtException', () => {
+  console.log(`[Server] Uncaught Exception is detected, shutting down...⚡`);
+  process.exit(1);
+});
