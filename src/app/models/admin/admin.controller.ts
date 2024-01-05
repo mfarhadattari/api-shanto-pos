1aea0599fe4086fc4558bc2ba39e186cc32fbeeb
Mohammad Farhad <mfarhad.attari@gmail.com>
2024-01-05 21:23:30 +0600

    Implemented catchAsync function

--- src/app/models/admin/admin.controller.ts
+++ src/app/models/admin/admin.controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from 'express';
+import catchAsync from '../../utils/catchAsync';
 import { AdminServices } from './admin.service';
 
 // ---------------->> Create Admin Controller <<-----------------
-const createAdmin = async (req: Request, res: Response) => {
+const createAdmin = catchAsync(async (req: Request, res: Response) => {
   const result = await AdminServices.createAdmin(req.body);
   res.status(200).send(result);
-};
+});
 
 // ---------------->> Export Admin Controllers <<-----------------
 export const AdminControllers = { createAdmin };
