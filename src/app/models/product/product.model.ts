49c416ff9977f19df138b8f4eb2212fb5aff8e9e
Mohammad Farhad <mfarhad.attari@gmail.com>
2024-01-09 11:13:18 +0600

    Product model implemented

---
+++ src/app/models/product/product.model.ts
@@ -0,0 +1,40 @@
+import { Schema, model } from 'mongoose';
+import { IProduct } from './product.interface';
+
+const productSchema = new Schema<IProduct>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    categoryId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Product = model<IProduct>('Product', productSchema);
